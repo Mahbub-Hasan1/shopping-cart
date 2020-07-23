4a601f122bd2c6f55bb858ec82abe22e2d40c6ff
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-07-23 12:23:34 +0600

    iPhone mines end

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ iPhone11BlackPlusBtn.addEventListener("click", function(){
     const iPhoneValue = iPhone11BlackValueNumber + 1;
 
     document.getElementById("iPhone11BlackValue").value = iPhoneValue;
-    
+
 });
+ // iPhone mines-
+const iPhone11BlackMinusBtn = document.getElementById("iPhone11BlackMinusBtn");
+iPhone11BlackMinusBtn.addEventListener("click", function(){
+    const iPhone11BlackValue = document.getElementById("iPhone11BlackValue").value;
+    const iPhone11BlackValueNumber = parseFloat(iPhone11BlackValue);
+    const iPhoneValue = iPhone11BlackValueNumber - 1;
+
+    document.getElementById("iPhone11BlackValue").value = iPhoneValue;
+})
